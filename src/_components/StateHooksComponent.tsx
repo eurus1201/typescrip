2ab06f122145560d63465a897656391ec41f9416
Your Name <you@example.com>
2020-04-06 16:41:09 +0430

    hello world

---
+++ src/_components/StateHooksComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface State {
+}
+
+const StateHooksComponent: React.FC<State> = ({}) => {
+
+    return (
+        <div>
+            <div>
+                Hello world
+            </div>
+            State Hooks component
+        </div>
+    )
+}
+
+export default StateHooksComponent;

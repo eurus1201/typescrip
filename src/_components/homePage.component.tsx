9d5a4073809a77bc31b175692ab770359306ba34
Your Name <you@example.com>
2020-04-10 16:12:21 +0430

    fixed bug

--- src/_components/homePage.component.tsx
+++ src/_components/homePage.component.tsx
@@ -1,80 +1,86 @@
 import React, { useEffect, useState } from "react";
-import { Grid, } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import MyCard from "./card.component";
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-interface user {
-    email: string;
-    firstname: string;
-    id: number;
-    avatar: string;
+interface IUser {
+  email: string;
+  firstname: string;
+  id: number;
+  avatar: string;
+  first_name: string;
+  last_name: string;
 }
 
-export const PhotoList : React.FC = () => {
-    const [users, setUsers] = useState<[user]>();
-    const [card,setCards] = useState(true);
+export const PhotoList: React.FC = () => {
+  const [users, setUsers] = useState<IUser[]>();
+  const [card, setCards] = useState({});
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCards({  [event.target.name]: event.target.checked });
-      };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCards({ [event.target.name]: !!event.target.checked });
+    // setCards(true);
+  };
 
-        async function fetchData() {
-        const res = await fetch('https://reqres.in/api/users');
-        res
-          .json()
-          .then(res => setUsers(res))
-          .catch(err => (console.log(err, 'error...'));
-      }
-    
-      useEffect(() => {
-        fetchData();
+  useEffect(() => {
+    fetch("https://reqres.in/api/users").then(res => {
+      res.json().then(usersRes => {
+        const data: IUser[] = usersRes.data;
+        setUsers(data);
       });
+    });
+  }, []);
 
-    return (
-        <div>
-<FormControlLabel
-        control={<Switch checked={state.checkedA} onChange={handleChange} name="cardMod" />}
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Switch checked={true} onChange={handleChange} name="cardMod" />
+        }
         label="Secondary"
       />
-            {card ? <div>
-                <h3>THE CARD STYLE</h3>
-                <Grid container spacing={10}
-                    style={{ padding: '24px' }}
-                >
-                    {!!users && users.map(user =>
-                        <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
-                            <MyCard
-                                key={user.id} email={user.email} firstname={user.first_name}
-                                lastname={user.last_name} avatar={user.avatar}
-                            />
-                        </Grid>)}
+      {card ? (
+        <div>
+          <h3>THE CARD STYLE</h3>
+          <Grid container spacing={10} style={{ padding: "24px" }}>
+            {!!users &&
+              users.map(user => (
+                <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+                  <MyCard
+                    key={user.id}
+                    email={user.email}
+                    firstname={user.first_name}
+                    lastname={user.last_name}
+                    avatar={user.avatar}
+                  />
                 </Grid>
-            </div> : <div>
-                    <h3>THE LIST STYLE</h3>
-                    <Grid>
-                        {!!users && users.map(user =>
-                            <div key={user.id}>
-
-                                <div>
-                                    <span>it is email:{user.email}</span>
-                                </div>
-                                <div>
-                                    <span > it is firstname:{user.first_name}</span>
-                                </div>
-                                <div>
-                                    <span > it is lastname:{user.last_name}</span>
-                                </div>
-                                <div>
-                                    <span > it is avatar:{user.avatar}</span>
-                                </div>
-
-                            </ div>
-                        )}
-                    </Grid>
-                </div>
-
-            }
+              ))}
+          </Grid>
         </div>
-    )
-}
+      ) : (
+        <div>
+          <h3>THE LIST STYLE</h3>
+          <Grid>
+            {!!users &&
+              users.map(user => (
+                <div key={user.id}>
+                  <div>
+                    <span>it is email:{user.email}</span>
+                  </div>
+                  <div>
+                    <span> it is firstname:{user.first_name}</span>
+                  </div>
+                  <div>
+                    <span> it is lastname:{user.last_name}</span>
+                  </div>
+                  <div>
+                    <span> it is avatar:{user.avatar}</span>
+                  </div>
+                </div>
+              ))}
+          </Grid>
+        </div>
+      )}
+    </div>
+  );
+};
